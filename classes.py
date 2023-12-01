36974f8f075439bdf1386f8ef311f3393168baee
Fernando Mello de Barros <141685774+fernandomellob@users.noreply.github.com>
2023-12-01 11:31:56 -0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,55 @@
+class Player
+  def Converter(musica):
+    return 0
+
+  def Play_musica(play_musica):
+    return 0
+
+  def Reset_musica(reset_musica)
+    return 0
+
+  def Altera_velocidade(velocidade_musica):
+    return 0
+
+  def esta_tocando():
+    return 0
+
+  def esta_parada():
+    return 0
+
+  def esta_acelarada():
+    return 0
+
+  def esta_tom_acelerado():
+    return 0
+
+class CampoDeTexto
+  def Altera_texto(texto):
+    return 0
+  def get_texto():
+    return 0
+
+class Musica
+  def Altera_volume(volume):
+    return 0
+
+  def Altera_velocidade(velocidade):
+    return 0
+
+  def Altera_tom(tom)
+    return 0
+
+  def get_volume()
+    return 0
+
+  def get_tom():
+    return 0
+
+  def get_velocidade():
+    return 0
+
+  def_texto_musica():
+    return 0
+
+
+  
