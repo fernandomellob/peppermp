e93c9c336020e03bb6181d03716dc80f6a4b9451
Guillermo Nogueira <64170683+GuillermoLN@users.noreply.github.com>
2024-01-28 08:07:19 -0300

    Adicionadas classe Música e classes para tradução MIDI
    Ainda falta adicionar o caso em que nenhum dos caracteres é lido e o caso do telefone, que não entendi o que foi definido, assim, o código referenciado com "telefone" não é relevante.
    
    Optei por mudar a classe de tradução e criar várias classes com métodos para cada tipo de alteração, assim fica mais claro e as definições ficam mais separadas

--- classes.py
+++ classes.py
@@ -1,4 +1,10 @@
-class Player
+import time
+import pygame
+import random
+from midiutil import MIDIFile
+
+
+class Player:
   def Converter(musica):
     return 0
 
@@ -23,33 +29,286 @@ class Player
   def esta_tom_acelerado():
     return 0
 
-class CampoDeTexto
+class CampoDeTexto:
   def Altera_texto(texto):
     return 0
   def get_texto():
     return 0
 
-class Musica
-  def Altera_volume(volume):
-    return 0
+#Métodos para tratar os casos de string que mudam o tom da musica
+class StringOitava:
+    __CHARAUMENTA = "+"
+    __CHARDIMINUI = "-"
+    __CHARFIXO = "R"
+    __CARACTERESEFEITO = __CHARAUMENTA,__CHARDIMINUI
+    __OITAVA = 12
+    __MAXTOM = 3 * __OITAVA
 
-  def Altera_velocidade(velocidade):
-    return 0
+    @staticmethod
+    def ehString( stringMusica, i):
+        if i + 1 < len(stringMusica) and stringMusica[i: i + 1] == StringOitava.__CHARFIXO and stringMusica[i + 1] in StringOitava.__CARACTERESEFEITO:
+             return True
+        else:
+            return False
+        
+    @staticmethod
+    def setOitava(self, tomAtual, stringMusica, i):
+        caractere = stringMusica[i + 1]
+        match caractere:
+            case "+":       
+                if tomAtual < StringOitava.__MAXTOM:
+                    return tomAtual + StringOitava.__OITAVA
+                else:
+                    return tomAtual
+            case "-":
+                if tomAtual > -StringOitava.__MAXTOM:
+                     return tomAtual - StringOitava.__OITAVA 
+                else:
+                     return tomAtual
+                
+#Método para mudar o BPM da Música a partir de uma String                
+class StringBPM():
+    __STRINGFIXA = "BPM+"
+    __INCREMENTOBPM = 80
 
-  def Altera_tom(tom)
-    return 0
+    @staticmethod
+    def ehString(stringMusica, i):
+        if stringMusica[i: i + len(StringBPM.__STRINGFIXA)] == StringBPM.__STRINGFIXA:
+             return True
+        else:
+            return False
 
-  def get_volume()
-    return 0
+    @staticmethod
+    def setBPM(bpmAtual):
+        return bpmAtual + StringBPM.__INCREMENTOBPM
 
-  def get_tom():
-    return 0
+# Super Classe de Notas, para organizar os métodos e definicoes comuns entre elas
+class Notas:
 
-  def get_velocidade():
-    return 0
+    LA = 69
+    RE = 62
+    MI = 64
+    FA = 63
+    SOL = 67
+    DO = 60
+    SI = 71
+    CaractereLa = "A"
+    CaractereSi = "B"
+    CaractereDo = "C"
+    CaractereRe = "D"
+    CaractereMi = "E"
+    CaractereFa = "F"
+    CaractereSol = "G"
+     
 
-  def_texto_musica():
-    return 0
+    def __init__(self, caracteresEfeito):
+        self.caracteresEfeito = caracteresEfeito
+    def ehCaractere(self, caractereMusica):
+         return caractereMusica.lower() in self.caracteresEfeito.lower()
 
+#Clase para selecionar o caso em que a nota tocada deve ser a anterior
+class NotasRepetidas(Notas):
+    __TOMTELEFONE = 84
+    __NOTAS = [Notas.CaractereLa, Notas.CaractereSi, Notas.CaractereDo, Notas.CaractereRe, Notas.CaractereMi, Notas.CaractereFa, Notas.CaractereSol]
+
+    def verifUltimoCharNota(self, caractereMusica):
+        if caractereMusica in self.__NOTAS:
+            return True
+        else:
+            return False    
+    
+    def getNota(self, caractereMusica):
+        match caractereMusica:
+            case self.CaractereDo:
+                return self.DO
+            case self.CaractereRe:
+                return self.RE
+            case self.CaractereMi:
+                return self.MI
+            case self.CaractereFa:
+               return self.FA
+            case self.CaractereSol:
+                return self.SOL
+            case self.CaractereLa:
+                return self.LA
+            case self.CaractereSi:                    
+                return self.SI
+            case _:
+                return self.__TOMTELEFONE
+
+# Classe para selecionar as notas                 
+class NotasBasicas(Notas):
+    __SEMSOM = 128
+    CaractereSemSom = " "
+
+    def getNota(self, caractereMusica):
+        match caractereMusica:
+            case self.CaractereDo:
+                return self.DO
+            case self.CaractereRe:
+                return self.RE
+            case self.CaractereMi:
+                return self.MI
+            case self.CaractereFa:
+                return self.FA
+            case self.CaractereSol:
+                return self.SOL
+            case self.CaractereLa:
+                return self.LA
+            case self.CaractereSi:
+                return self.SI
+            case " ":
+                return self.__SEMSOM  
+
+# Classe para selecionar as notas aleatórias da música
+class NotaAleatoria(Notas):
+    def getNota(self):
+        return random.choice([self.LA, self.RE, self.MI, self.FA, self.SOL, self.DO, self.SI])
+
+#Método para alterar o volume
+class Volume():
+    __AUMENTARVOLUME = "+"
+    __DIMINUIRVOLUME = '-'
+
+    @staticmethod
+    def ehCaractere(caractereMusica):
+         return caractereMusica in [Volume.__AUMENTARVOLUME, Volume.__DIMINUIRVOLUME]
+    @staticmethod
+    def setVolume(caractereVolume, volume, volumePadrao, volumeMax):
+        if caractereVolume == Volume.__AUMENTARVOLUME:
+            volume = volume * 2
+            if volume > volumeMax:
+                return volumeMax
+            else:
+                return volume
+        elif caractereVolume == Volume.__DIMINUIRVOLUME:
+            return volumePadrao
+
+class Instrumentos():
+
+    @staticmethod
+    def ehCaractere(caractereMusica):
+         return caractereMusica == "\n"   
+    
+    @staticmethod
+    def getInstrumentos():
+          return random.randint(1,127)
+
+class BPM():
+    @staticmethod
+    def ehCaractere(caractereMusica):
+         return caractereMusica == ";" 
+    
+    @staticmethod
+    def setBPM():
+         return random.randint(1,1000)
+      
+class Musica:
+#Classe geral da música, responsável por gerar o arquivo de música
+# A Classe solicita informações sobre o caractere para a classe TraducaoCaratereMIDI e faz as devidas alteracoes no arquivo de musica
+    __TRACK = 0
+    __CANAL = 0
+    __DURACAO = 1
+    __BPMBASE = 120
+    __VOLUMEBASE = 50
+    __VOLUMEMAX = 127
+
+    notaBase = NotasBasicas("ABCDEFG ")
+    notaAleatoria = NotaAleatoria("?")
+    notaRepetida = NotasRepetidas("OIU")
+
+    def __init__(self):
+        self.midi = MIDIFile(1)
+        self.bpm = self.__BPMBASE
+        self.volume = self.__VOLUMEBASE
+        self.tempo_musica = 0
+        self.tom = 0
+        self.instrumento = 0
+       
+    #Salva arquivo
+    def criarArquivoMidi(self):
+        midi_file = "output.mid"
+
+        with open(midi_file, "wb") as midi_output:
+            self.midi.writeFile(midi_output)
+
+    #Adiciona a nota e corrige o tempo
+    def adicionarNota(self, numeroNota):
+        self.midi.addNote(self.__TRACK, self.__CANAL, numeroNota + self.tom,  self.tempo_musica, self.__DURACAO, self.volume)
+        self.tempo_musica += 1
+  
+    #Função para adicionar o som do telefone, que nao encontrei como fazer
+    def adicionarTelefone(self):
+        self.midi.addNote(self.__TRACK, self.__CANAL, 84,  self.tempo_musica, self.__DURACAO, self.volume)
+    
+    def alteraVolume(self, novoVolume):
+        self.volume = novoVolume
+
+    def alteraBPM(self, novoBPM):
+        self.bpm = novoBPM
+        self.midi.addTempo(self.__TRACK, self.tempo_musica, self.bpm)
+
+    def alteraTom(self, novoTom):
+        self.tom = novoTom
+
+    def alteraInstrumento(self, novoInstrumento):
+        self.instrumento = novoInstrumento
+        self.midi.addProgramChange(self.__TRACK, self.__CANAL,  self.tempo_musica, self.instrumento)
+        
+    def criarMusica(self, stringNotas):
+
+        self.midi.addTempo(self.__TRACK, 0 , self.bpm)
+        caracteresParaPular = 0
+
+        for i, caractere in enumerate(stringNotas):
+
+            if caracteresParaPular > 0:
+                caracteresParaPular = caracteresParaPular - 1     
+
+            elif StringBPM.ehString(stringNotas, i):
+                self.alteraBPM(StringBPM.setBPM(self.bpm))
+                caracteresParaPular = 3
+
+            elif StringOitava.ehString(stringNotas, i):
+                self.alteraTom(StringOitava.setOitava(self.tom, stringNotas, i))
+                caracteresParaPular = 1
+
+            elif self.notaRepetida.ehCaractere(caractere):
+                if self.notaRepetida.verifUltimoCharNota(ultimoCaractere) == True: 
+                    self.adicionarNota(self.notaRepetida.getNota(ultimoCaractere))
+                else:
+                    self.adicionarTelefone()
+
+            elif self.notaBase.ehCaractere(caractere):
+                self.adicionarNota(self.notaBase.getNota(caractere))
+
+            elif self.notaAleatoria.ehCaractere(caractere):
+                self.adicionarNota(self.notaAleatoria.getNota())      
+
+            elif Volume.ehCaractere(caractere):
+                self.alteraVolume(Volume.setVolume(caractere, self.volume, self.__VOLUMEBASE, self.__VOLUMEMAX))
+            
+            elif Instrumentos.ehCaractere(caractere):
+                self.alteraInstrumento(Instrumentos.getInstrumentos())
+            
+            elif BPM.ehCaractere(caractere):
+                self.alteraBPM(BPM.setBPM)
+            
+            ultimoCaractere = caractere
+
+        self.criarArquivoMidi()
+
+
+def tocar_musica(midi_file):
+    #Funcao para tocar o arquivo
+    pygame.init()
+    pygame.mixer.init()
+
+    try:
+        pygame.mixer.music.load(midi_file)
+        pygame.mixer.music.play()
+        pygame.event.wait()
+    except pygame.error as e:
+        print("Pygame error:", e)
 
   
