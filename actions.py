4f8b81738dd2570de4c96f58d00db3b71cc310b8
Guillermo Nogueira <64170683+GuillermoLN@users.noreply.github.com>
2024-01-29 18:58:43 -0300

    Diminuição das atribuições de actions

--- actions.py
+++ actions.py
@@ -1,23 +1,17 @@
 import functions
-import pygame
-import tkinter.messagebox as mb
 
 def play(text,volume,oitava,doisx):
     musica = functions.Musica(volume, oitava, doisx)
     musica.criarMusica(text)
-    functions.tocar_musica("output.mid")
-    print("play")
+    functions.Player.tocarMusica("output.mid")
 
 def reset():
-    pygame.mixer.music.stop()
-    print("reset")
+    functions.Player.pararMusica()
 
 def ajuda():
-    mb.showinfo("Ajuda", "O campo de texto serve para você digitar a música que deseja tocar.\n\nO botão ▶ serve para tocar a música.\n\nO botão ⏺️ serve para parar a música.\n\nO checkbox 2x serve para tocar a música duas vezes mais rápido.\n\nO checkbox +# serve para tocar a música uma oitava acima.\n\nO botão ? serve para mostrar essa mensagem de ajuda.\n\nO botão ⬇ serve para salvar a música em um arquivo midi.")
-    print("?")
+    functions.mensagemAjuda()
 
 def salvarMidi(text):
     musica = functions.Musica()
     musica.criarMusica(text)
-    print("salvarMidi")
 
